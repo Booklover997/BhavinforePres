6c2833264ad9cb18c54ca0c5896c11b168e2ca32
Booklover997 <rishireddy997@gmail.com>
2023-09-18 21:14:27 -0400

    On click the text of the button will change from work to stop

--- app/src/main/java/com/example/bhavinforpresident/MainActivity.java
+++ app/src/main/java/com/example/bhavinforpresident/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.bhavinforpresident;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button_start_reset;
+    private TextView timer_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_start_reset = findViewById(R.id.button_start_reset);
+        button_start_reset.setOnClickListener(view -> {
+            if (button_start_reset.getText().equals("Work")){
+                button_start_reset.setText("Stop");
+            }else{
+                button_start_reset.setText("Work");
+            }
+        });
     }
 }
