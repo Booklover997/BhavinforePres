9ff3004350771eb11292d283b45cc82fdc8b15ed
Booklover997 <rishireddy997@gmail.com>
2023-09-18 21:31:38 -0400

    On click turns the button to red or green depending on its current state

--- app/src/main/java/com/example/bhavinforpresident/MainActivity.java
+++ app/src/main/java/com/example/bhavinforpresident/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.bhavinforpresident;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +19,11 @@ public class MainActivity extends AppCompatActivity {
         button_start_reset.setOnClickListener(view -> {
             if (button_start_reset.getText().equals("Work")){
                 button_start_reset.setText("Stop");
+                button_start_reset.setBackgroundColor(Color.RED);
             }else{
                 button_start_reset.setText("Work");
+                button_start_reset.setBackgroundColor(Color.GREEN);
+
             }
         });
     }
