bfdcaabd94f341cb5b1dae0770ca9df8e97d6039
Booklover997 <rishireddy997@gmail.com>
2023-09-18 21:42:59 -0400

    Fixed errors in code due to not using res/values/string.xml file

--- app/src/main/java/com/example/bhavinforpresident/MainActivity.java
+++ app/src/main/java/com/example/bhavinforpresident/MainActivity.java
@@ -4,24 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button_start_reset;
-    private TextView timer_text;
+//    private TextView timer_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button_start_reset = findViewById(R.id.button_start_reset);
         button_start_reset.setOnClickListener(view -> {
-            if (button_start_reset.getText().equals("Work")){
-                button_start_reset.setText("Stop");
+            if (button_start_reset.getText().equals(getResources().getString(R.string.work))){
+                button_start_reset.setText(getResources().getString(R.string.stop));
                 button_start_reset.setBackgroundColor(Color.RED);
             }else{
-                button_start_reset.setText("Work");
+                button_start_reset.setText(getResources().getString(R.string.work));
                 button_start_reset.setBackgroundColor(Color.GREEN);
 
             }
