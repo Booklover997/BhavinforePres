d05dea97340a562d81d1e8cb04360b498fe0dfbf
Booklover997 <65503002+Booklover997@users.noreply.github.com>
2023-09-20 23:13:30 -0400

    Created the timer text and added code that refernces it and initalizes the variable "timer_text" to it

--- app/src/main/java/com/example/bhavinforpresident/MainActivity.java
+++ app/src/main/java/com/example/bhavinforpresident/MainActivity.java
@@ -7,13 +7,19 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
     private Button button_start_reset;
-//    private TextView timer_text;
+    private TextView timer_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer_text = findViewById(R.id.time);
+
+
         button_start_reset = findViewById(R.id.button_start_reset);
         button_start_reset.setOnClickListener(view -> {
             if (button_start_reset.getText().equals(getResources().getString(R.string.work))){
